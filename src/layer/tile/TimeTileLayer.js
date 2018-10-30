48e97e28eda2f35a74ac9a4f7b463bbd933398f4
Charlton Galvarino <charlton@2creek.com>
2018-10-30 05:56:41 -0700

    spacing and comment

--- src/layer/tile/TimeTileLayer.js
+++ src/layer/tile/TimeTileLayer.js
@@ -7,6 +7,7 @@ import {TileLayer} from './TileLayer';
  * Currently this is a stub to treat Oceansmap TimeTileLayers as normal TileLayers.  This extension was
  * originally in the OM code base and did things like paint the layer as one tile instead of smaller tiles.
  * And it kept the current timestamp on the map while loading a new user-requested time.
+ * https://github.com/asascience/OceansMap/blob/e94e697c8cebe97f077e74af1251f964ab1f69ca/oceansmap/static/js/common/L.TimeTileLayer.js
  */
 
 export var TimeTileLayer = TileLayer.extend({
@@ -17,5 +18,5 @@ export var TimeTileLayer = TileLayer.extend({
 // Instantiates a time tile layer object given a `URL template` and optionally an options object.
 
 export function timeTileLayer(url, options) {
-        return new TimeTileLayer(url, options);
+	return new TimeTileLayer(url, options);
 }
